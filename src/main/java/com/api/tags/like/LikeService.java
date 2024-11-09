e28abbf13406112c55dbcf2e342825ef32861e75
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-11-09 19:24:34 -0300

    Criação de método para curtir e descurtir postagem

--- src/main/java/com/api/tags/like/LikeService.java
+++ src/main/java/com/api/tags/like/LikeService.java
@@ -1,5 +1,43 @@
 package com.api.tags.like;
 
-public class LikeService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.api.tags.like.definition.LikeModel;
+import com.api.tags.like.repository.LikeRepository;
+import com.api.tags.post.definition.PostModel;
+import com.api.tags.post.repository.PostRepository;
+import com.api.tags.user.definition.UserModel;
+import com.api.tags.user.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class LikeService {
+	@Autowired
+    private LikeRepository likeRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Transactional
+    public void toggleLike(String userId, String postId) {
+
+        UserModel user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado."));
+        PostModel post = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("Postagem não encontrada."));
+
+        if (likeRepository.existsByUserAndPost(user, post)) {
+            likeRepository.deleteByUserAndPost(user, post);
+        } else {
+            LikeModel like = new LikeModel();
+            like.setUser(user);
+            like.setPost(post);
+            likeRepository.save(like);
+        }
+    }
 }
--- src/main/java/com/api/tags/like/repository/LikeRepository.java
+++ src/main/java/com/api/tags/like/repository/LikeRepository.java
@@ -1,5 +1,13 @@
 package com.api.tags.like.repository;
 
-public interface LikeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.api.tags.like.definition.LikeModel;
+import com.api.tags.post.definition.PostModel;
+import com.api.tags.user.definition.UserModel;
+
+public interface LikeRepository extends JpaRepository<LikeModel, String> {
+	boolean existsByUserAndPost(UserModel user, PostModel post);
+	
+	void deleteByUserAndPost(UserModel user, PostModel post);
 }
