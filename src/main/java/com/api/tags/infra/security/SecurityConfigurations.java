38b4cb0fd9cb9d2334fdca7e13e90e2a963a1361
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-11-28 20:48:19 -0300

    Adicionando configuração para Prometheus e Grafana

--- src/main/java/com/api/tags/infra/security/SecurityConfigurations.java
+++ src/main/java/com/api/tags/infra/security/SecurityConfigurations.java
@@ -39,6 +39,7 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                		.requestMatchers("/actuator/**").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
 //						.requestMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
