7f5f6e9da0042465e9e974f3144c7c5d2e2dc864
Evelyn Bittelbrunn <74337638+evelynbittelbrunn@users.noreply.github.com>
2024-11-29 00:45:01 -0300

    Configuração do Swagger

--- src/main/java/com/api/tags/infra/security/SecurityConfigurations.java
+++ src/main/java/com/api/tags/infra/security/SecurityConfigurations.java
@@ -40,6 +40,7 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                 		.requestMatchers("/actuator/**").permitAll()
+                		.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
 						.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
 //						.requestMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
